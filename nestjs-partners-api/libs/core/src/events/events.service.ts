181778447ce3c3cb9b7bf8f1202f8b2a9d1981f4
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-06-18 09:07:20 -0300

    init core app

--- nestjs-partners-api/libs/core/src/events/events.service.ts
+++ nestjs-partners-api/libs/core/src/events/events.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'libs/core/src/prisma/prisma.service';
 import { ReserveSpotDto } from './dto/reserve-spot.dto';
 import { Prisma, SpotStatus, TicketStatus } from '@prisma/client';
 
---
+++ nestjs-partners-api/libs/core/src/spots/spots-core.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { SpotsService } from './spots.service';
+@Module({
+  providers: [SpotsService],
+})
+export class SpotsCoreModule {}
